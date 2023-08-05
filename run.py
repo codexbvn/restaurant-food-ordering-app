58fe0e6840bfc8debee40a4c65e36db8b15850bd
codexbvn <80110118+codexbvn@users.noreply.github.com>
2023-08-05 13:52:35 +0530

    run.py updated

---
+++ run.py
@@ -0,0 +1,8 @@
+from backend.app import app as backend_app
+from frontend.app import app as frontend_app
+
+
+if __name__ == "__main__":
+    # Run the backend and frontend apps separately
+    backend_app.run(port=5000)
+    frontend_app.run(port=8000)
