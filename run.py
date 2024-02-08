fa0673c3ea1db4bd4596247cd92cb75426ecb47c
Bhuwan <80110118+codexbvn@users.noreply.github.com>
2024-02-08 21:40:00 +0530

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from frontend.app import app as frontend_app
 
 
 if __name__ == "__main__":
-    # Run the backend and frontend apps separately
+    
     backend_app.run(port=5000)
     frontend_app.run(port=8000)
 
